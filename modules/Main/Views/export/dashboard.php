665d1ca7b270c36ea200e07064465ffa28dbea58
Chonlawit penwijit <43345050+chonlawit-pe61@users.noreply.github.com>
2023-10-24 19:54:22 +0700

    ิbackup

--- modules/Main/Views/export/dashboard.php
+++ modules/Main/Views/export/dashboard.php
@@ -79,7 +79,7 @@
 	}
 
 	#resultsTableMarket1 {
-		height: 200px;
+		height: 190px;
 		width: 215px;
 		background: #73A0E0;
 		overflow: hidden;
@@ -88,7 +88,7 @@
 	}
 
 	#resultsTableMarket2 {
-		height: 200px;
+		height: 190px;
 		width: 215px;
 		background: #DDC354;
 		overflow: hidden;
@@ -117,7 +117,7 @@
 
 	#resultsTable4 {
 		height: 35px;
-		width: 200px;
+		width: 240px;
 		background: #73A0E0;
 		overflow: hidden;
 		border-radius: 28px !important;
@@ -127,7 +127,7 @@
 
 	#resultsTable5 {
 		height: 35px;
-		width: 200px;
+		width: 240px;
 		background: #DDC354;
 		overflow: hidden;
 		border-radius: 28px !important;
@@ -201,7 +201,7 @@
 									<table style="width: 100%;">
 										<tbody>
 											<tr style="border: 0;padding: 0px 0px;">
-												<td class="colorText" style="text-align: center;padding: 5px 10px ; font-size: 25px;font-weight: bold;width: 100%;">
+												<td class="" style="color: white;text-align: center;padding: 5px 10px ; font-size: 25px;font-weight: bold;width: 100%;">
 													<?php echo number_format($SumDateData); ?> คน
 												</td>
 											</tr>
@@ -264,16 +264,17 @@
 											<div style="border: 0; padding-bottom: 0px; ">
 												<div id="" style="padding-left: 10px; padding-right: 10px; ">
 													<table style="width: 100%;">
-														<tr style="border: 0;">
+														<tr style="">
 															<td style="width: 15%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 																<img class="img-profile rounded-circle" src="<?php echo $flag ?>" style="height: 34px; ">
 															</td>
-															<td style="text-align: left; padding: 4px 15px;color: white; font-size: 16px;font-weight: bold; width: 55%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid #FFFFFF' ?>;">
+															<td style="text-align: left; padding: 6.5px 15px;color: white; font-size: 16px;font-weight: bold; width: 75%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid #FFFFFF' ?>;">
 																<?php echo $v['COUNTRY_NAME_EN'] ?>
 																<br>
 																<?php echo number_format($v['NUM']); ?> คน
 															</td>
-															<td style="border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+															<td style="border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;text-align: center;">
+																<img src="<?php echo base_url('public/img/arrowup1.png') ?>" alt="">
 															</td>
 														</tr>
 													</table>
@@ -297,16 +298,17 @@
 											<div style="border: 0; padding-bottom: 0px; ">
 												<div id="" style="padding-left: 10px; padding-right: 10px; ">
 													<table style="width: 100%;">
-														<tr style="border: 0;">
+														<tr style="">
 															<td style="width: 15%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 																<img class="img-profile rounded-circle" src="<?php echo $flag ?>" style="height: 34px; ">
 															</td>
-															<td style="text-align: left; padding: 4px 15px;color: white; font-size: 16px;font-weight: bold; width: 70%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid #FFFFFF' ?>;">
+															<td class="colorText" style="text-align: left; padding: 6.5px 15px; font-size: 16px;font-weight: bold; width: 75%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid #FFFFFF' ?>;">
 																<?php echo $v['COUNTRY_NAME_EN'] ?>
 																<br>
 																<?php echo number_format($v['NUM']); ?> คน
 															</td>
-															<td style="border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+															<td style="border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;text-align: center;">
+																<img src="<?php echo base_url('public/img/arrowup1.png') ?>" alt="">
 															</td>
 														</tr>
 													</table>
@@ -324,11 +326,11 @@
 			</div>
 		</div>
 		<div class="col6" style="margin-left: 20px;">
-			<table style="width: 100%;padding-bottom: 25px;">
+			<table style="width: 100%;padding-bottom: 10px;">
 				<thead>
 					<tr>
 						<td style="width: 5%;text-align: center;">
-							<img src="<?php echo base_url('public/img/amazing-th.png') ?>" alt="" style="width: 100px; padding-left: 35px;">
+							<img src="<?php echo base_url('public/img/amezingThai.png') ?>" alt="" style="width: 100px; padding-left: 35px;">
 						</td>
 						<td style="width: 5%; text-align: center;margin: auto 0px;">
 							<img src="<?php echo base_url('public/img/TATIC-Logo.png') ?>" alt="" style="width: 100px;">
@@ -339,11 +341,11 @@
 			<img src="<?php echo base_url('public/uploads/main/' . $to_date . 'chart_daily.png') ?>" style="width:100%;height:160px;">
 
 			<div class="col12" style="">
-				<div class="colorTextLeft " style="text-align: center;font-size: 16px;font-weight: bold; ">จำนวนนักท่องเที่ยว ตลาดระยะใกล้ 5 อันดับแรก</div>
+				<div class="colorTextLeft" style="text-align: center;font-size: 18px;font-weight: bold; ">จำนวนนักท่องเที่ยว ตลาดระยะใกล้ 5 อันดับแรก</div>
 			</div>
 			<div>
 				<div class="col6">
-					<p class="colorTextLeft" style="text-align: center;margin:0px; font-weight: bold;"><?php echo $Mydate->date_eng2thai($to_date, 543) ?></p>
+					<p class="colorTextLeft" style="font-size: 15px;text-align: center;margin:0px; font-weight: bold;"><?php echo $Mydate->date_eng2thai($to_date, 543) ?></p>
 					<div id="resultsTableMarket1" style=" flex-direction: row; padding: 0px 15px;">
 						<?php $c = 0;
 						foreach ($SumMarketDate['Short'] as $v) {
@@ -357,16 +359,17 @@
 							<div style="border: 0; ">
 								<div id="">
 									<table style="width: 100%;">
-										<tr style="border: 0;">
+										<tr style="">
 											<td style="width: 10%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 												<img class="img-profile rounded-circle" src="<?php echo $flag ?>" style="height: 34px; ">
 											</td>
-											<td style="text-align: left; padding: 4px 15px;color: #193666; font-size: 14px;font-weight: bold; width: 70%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+											<td style="text-align: left; padding: 4.1px 15px;color: white; font-size: 14px;font-weight: bold; width: 75%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 												<?php echo $v['COUNTRY_NAME_EN'] ?>
 												<br>
 												<?php echo number_format($v['NUM']); ?> คน
 											</td>
-											<td style="font-size: 14px; color: #193666; font-weight: bold; width: 15%; text-align: left;border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+											<td style="font-size: 14px; color: #193666; font-weight: bold; width: 15%; text-align: center;border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+												<img src="<?php echo base_url('public/img/arrowup1.png') ?>" alt="">
 											</td>
 										</tr>
 									</table>
@@ -377,7 +380,7 @@
 					</div>
 				</div>
 				<div class="col6">
-					<p class="colorTextLeft" style="text-align: center;margin:0px; font-weight: bold;">สะสม <?php echo $Mydate->date_eng2thai($start_date_label, 543, 'S', 'S') ?> - <?php echo $Mydate->date_eng2thai($to_date, 543, 'S', 'S') ?></p>
+					<p class="colorTextLeft" style="font-size: 15px;text-align: center;margin:0px; font-weight: bold;">สะสม <?php echo $Mydate->date_eng2thai($start_date_label, 543, 'S', 'S') ?> - <?php echo $Mydate->date_eng2thai($to_date, 543, 'S', 'S') ?></p>
 
 					<div id="resultsTableMarket2" style=" flex-direction: row; padding: 0px 15px;">
 
@@ -393,16 +396,17 @@
 							<div style="border: 0; ">
 								<div id="">
 									<table style="width: 100%;">
-										<tr style="border: 0;">
+										<tr style="">
 											<td style="width: 10%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 												<img class="img-profile rounded-circle" src="<?php echo $flag ?>" style="height: 34px; ">
 											</td>
-											<td style="text-align: left; padding: 4px 15px;color: #193666; font-size: 14px;font-weight: bold; width: 70%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+											<td style="text-align: left; padding: 4.1px 15px;color: #193666; font-size: 14px;font-weight: bold; width: 75%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 												<?php echo $v['COUNTRY_NAME_EN'] ?>
 												<br>
 												<?php echo number_format($v['NUM']); ?> คน
 											</td>
-											<td style="font-size: 14px; color: #193666; font-weight: bold; width: 15%; text-align: left;border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+											<td style="font-size: 14px; color: #193666; font-weight: bold; width: 15%; text-align: center;border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+												<img src="<?php echo base_url('public/img/arrowup1.png') ?>" alt="">
 											</td>
 										</tr>
 									</table>
@@ -417,8 +421,8 @@
 				<table style="width: 100%;">
 					<tbody>
 						<tr style="border: 0;">
-							<td colspan="3" style="text-align: center;padding: 0px 10px; margin: auto 0px;">
-								<p id="" class="" style="padding-top: 2px;padding-bottom: 7px; margin: 0px;font-weight:bold; font-size: 16px; color: #CC409E;">จำนวนนักท่องเที่ยว ตลาดระยะไกล 5 อันดับแรก</p>
+							<td colspan="3" style="text-align: center;padding: 0px 5px; margin: auto 0px;">
+								<p id="" class="" style="padding-top: 2px;padding-bottom: 7px; margin: 0px;font-weight:bold; font-size: 18px; color: #CC409E;">จำนวนนักท่องเที่ยว ตลาดระยะไกล 5 อันดับแรก</p>
 							</td>
 						</tr>
 					</tbody>
@@ -426,7 +430,7 @@
 			</div>
 			<div>
 				<div class="col6">
-					<p class="" style="text-align: center;margin:0px; font-weight: bold; color: #CC409E;"><?php echo $Mydate->date_eng2thai($to_date, 543) ?></p>
+					<p class="" style="font-size: 15px;text-align: center;margin:0px; font-weight: bold; color: #CC409E;"><?php echo $Mydate->date_eng2thai($to_date, 543) ?></p>
 
 					<div id="resultsTableMarket1" style=" flex-direction: row; padding: 0px 15px;">
 
@@ -442,16 +446,17 @@
 							<div style="border: 0; ">
 								<div id="">
 									<table style="width: 100%;">
-										<tr style="border: 0;">
+										<tr>
 											<td style="width: 10%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 												<img class="img-profile rounded-circle" src="<?php echo $flag ?>" style="height: 34px; ">
 											</td>
-											<td style="text-align: left; padding: 4px 15px;color: #193666; font-size: 14px;font-weight: bold; width: 70%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+											<td style="text-align: left; padding: 4.1px 15px;color: white; font-size: 14px;font-weight: bold; width: 75%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 												<?php echo $v['COUNTRY_NAME_EN'] ?>
 												<br>
 												<?php echo number_format($v['NUM']); ?> คน
 											</td>
-											<td style="font-size: 14px; color: #193666; font-weight: bold; width: 15%; text-align: left;border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+											<td style="font-size: 14px; color: #193666; font-weight: bold; width: 15%; text-align: center;border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+												<img src="<?php echo base_url('public/img/arrowup1.png') ?>" alt="">
 											</td>
 										</tr>
 									</table>
@@ -462,7 +467,7 @@
 					</div>
 				</div>
 				<div class="col6">
-					<p class="" style="text-align: center;margin:0px; font-weight: bold; color: #CC409E;">สะสม <?php echo $Mydate->date_eng2thai($start_date_label, 543, 'S', 'S') ?> - <?php echo $Mydate->date_eng2thai($to_date, 543, 'S', 'S') ?></p>
+					<p class="" style="font-size: 15px;text-align: center;margin:0px; font-weight: bold; color: #CC409E;">สะสม <?php echo $Mydate->date_eng2thai($start_date_label, 543, 'S', 'S') ?> - <?php echo $Mydate->date_eng2thai($to_date, 543, 'S', 'S') ?></p>
 
 
 					<div id="resultsTableMarket2" style=" flex-direction: row; padding: 0px 15px;">
@@ -479,16 +484,17 @@
 							<div style="border: 0; ">
 								<div id="">
 									<table style="width: 100%;">
-										<tr style="border: 0;">
+										<tr>
 											<td style="width: 10%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 												<img class="img-profile rounded-circle" src="<?php echo $flag ?>" style="height: 34px; ">
 											</td>
-											<td style="text-align: left; padding: 4px 15px;color: #193666; font-size: 14px;font-weight: bold; width: 70%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+											<td style="text-align: left; padding: 4.1px 15px;color: #193666; font-size: 14px;font-weight: bold; width: 75%; border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
 												<?php echo $v['COUNTRY_NAME_EN'] ?>
 												<br>
 												<?php echo number_format($v['NUM']); ?> คน
 											</td>
-											<td style="font-size: 14px; color: #193666; font-weight: bold; width: 15%; text-align: left;border-bottom:  <?php echo $c == 10 ? '' : '1px solid white' ?>;">
+											<td style="font-size: 14px; color: #193666; font-weight: bold; width: 15%; text-align: center;border-bottom:<?php echo $c == 10 ? '' : '1px solid white' ?>;">
+												<img src="<?php echo base_url('public/img/arrowup1.png') ?>" alt="">
 											</td>
 										</tr>
 									</table>
@@ -501,11 +507,13 @@
 			</div>
 
 			<div style="position: absolute; top: 680px; right: 45px; width:100%;">
-				<p style="font-weight: bold; padding-top:5px; color: #fff; font-size:12.2px; color: #163868;">หมายเหตุ:ข้อมูลรายสัญชาติตัดข้อมูลผู้อพยพ,หน่วยงานพิเศษUN,ไม่มีสัญชาติออกทั้งนี้ตั้งแต่ 1 ม.ค. 66 ได้กำหนดหลักเกณฑ์การคำนวณนักท่องเที่ยวระหว่างประเทศเพิ่มเติม (สามารถอ่านเพิ่มเติมได้ที่นิยามในระบบฯ)</p>
+				<p style="font-weight: bold; padding-top:5px; color: #fff; font-size:12.3px; color: #163868;">หมายเหตุ:ข้อมูลรายสัญชาติตัดข้อมูลผู้อพยพ,หน่วยงานพิเศษUN,ไม่มีสัญชาติออกทั้งนี้ตั้งแต่
+					<br> 1 ม.ค.66 ได้กำหนดหลักเกณฑ์การคำนวณนักท่องเที่ยวระหว่างประเทศเพิ่มเติม (สามารถอ่านเพิ่มเติมได้ที่นิยามในระบบฯ)
+				</p>
 			</div>
 		</div>
 	</div>
-	<div style="position: absolute;top: 23%; left: 49%;rotate:-90;color: red; font-size: 20px;">
+	<div style="position: absolute;top: 23%; left: 48.5%;rotate:-90;color: red; font-size: 20px;">
 		ใช้เฉพาะภายใน ททท. เท่านั้น ¦ Internal Use Only
 	</div>
 </body>
